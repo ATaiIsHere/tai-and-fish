214cd15b9ed43ba4df98d2c81bffa33e8fadc64d
Sean Tai <ataiishere34@gmail.com>
2021-09-28 23:12:52 +0800

    add logo and change title

--- src/component/TopBlock.js
+++ src/component/TopBlock.js
@@ -1,9 +1,11 @@
 import LeftTimeDisplay from "./LeftTimeDisplay";
+import logo from "../img/logo.svg";
 
 const TopBlock = () => {
   return (
     <div className="top-block">
-        <h1 className="block-title white-text">{"Tai & Fish"}</h1>
+        <img className="logo" src={logo} />
+        <h1 className="block-title white-text">{"ATai & Fish"}</h1>
         <h2 className="block-text white-text">We Are Getting Married</h2>
         <LeftTimeDisplay />
     </div>
