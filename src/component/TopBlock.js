665c2a1c6e63bf6679812389c880b9c2f2b63ef9
Sean Tai <ataiishere34@gmail.com>
2021-09-28 23:20:40 +0800

    fix img error

--- src/component/TopBlock.js
+++ src/component/TopBlock.js
@@ -4,7 +4,7 @@ import logo from "../img/logo.svg";
 const TopBlock = () => {
   return (
     <div className="top-block">
-        <img className="logo" src={logo} />
+        <img className="logo" src={logo} alt="logo" />
         <h1 className="block-title white-text">{"ATai & Fish"}</h1>
         <h2 className="block-text white-text">We Are Getting Married</h2>
         <LeftTimeDisplay />
