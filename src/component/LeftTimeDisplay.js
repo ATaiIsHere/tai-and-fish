5237fe48da1c4551078f2b64c2176191a43fba4d
Sean Tai <ataiishere34@gmail.com>
2021-09-29 22:31:16 +0800

    let remain time work

--- src/component/LeftTimeDisplay.js
+++ src/component/LeftTimeDisplay.js
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import DigitalBall from "./DigitalBall";
 
+
+
 const LeftTimeDisplay = (props) => {
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [mins, setMins] = useState(0);
+  const [secs, setSecs] = useState(0);
+
+  useEffect(() => {
+    console.log(secs);
+    let repeat = setInterval(
+      () => {
+        let now = new Date();
+        let remainSeconds = (props.target.getTime() - now.getTime()) / 1000;
+
+        let remainDays = Math.floor(remainSeconds / 86400);
+        let remainHour = Math.floor(remainSeconds % 86400 / 3600);
+        let remainMins = Math.floor(remainSeconds % 3600 / 60);
+        let remainSecs = Math.floor(remainSeconds % 60);
+
+        setDays(remainDays);
+        setHours(remainHour);
+        setMins(remainMins);
+        setSecs(remainSecs);
+      }, 1000
+    );
+
+    return () => {
+      clearInterval(repeat);
+    }
+    
+  }, []);
+
   return (
     <div className="lefttime-display">
-      <DigitalBall digital={999} unit="DAYS" />
-      <DigitalBall digital={59} unit="HOURS" />
-      <DigitalBall digital={59} unit="MINUTES" />
-      <DigitalBall digital={59} unit="SECONDS" />
+      <DigitalBall digital={days} unit="DAYS" />
+      <DigitalBall digital={hours} unit="HOURS" />
+      <DigitalBall digital={mins} unit="MINUTES" />
+      <DigitalBall digital={secs} unit="SECONDS" />
     </div>
   );
 };
--- src/component/TopBlock.js
+++ src/component/TopBlock.js
@@ -7,7 +7,7 @@ const TopBlock = () => {
         <img className="logo" src={logo} alt="logo" />
         <h1 className="block-title white-text">{"ATai & Fish"}</h1>
         <h2 className="block-text white-text">We Are Getting Married</h2>
-        <LeftTimeDisplay />
+        <LeftTimeDisplay target={new Date('2021/12/19 12:00:00')} />
     </div>
   );
 }
