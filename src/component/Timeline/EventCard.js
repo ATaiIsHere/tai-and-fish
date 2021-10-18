0593948c07d732fd0c3b0428283361d79bc6b120
Sean Tai <ataiishere34@gmail.com>
2021-10-19 00:54:28 +0800

    complete `Our Story` block

--- src/component/Timeline/EventCard.js
+++ src/component/Timeline/EventCard.js
@@ -1,10 +1,10 @@
 const EventCard = (props) => (
-  <div className="event-card" data-aos="zoom-in" data-aos-duration="700">
+  <div className={`event-card ${props.side}`} data-aos="zoom-in" data-aos-duration="700">
     <div className="event-photo" style={{backgroundImage: `url(${props.photo})`}}></div>
     <div className="event-msg">
-      <div>First We Meet</div>
-      <div>DECEMBER 25, 2015</div>
-      <div>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.{props.msg}</div>
+      <div>{props.title}</div>
+      <div>{props.date}</div>
+      <div>{props.msg}</div>
     </div>
   </div>
 );
--- src/component/Timeline/Timeline.js
+++ src/component/Timeline/Timeline.js
@@ -4,7 +4,13 @@ import EventPhote1 from "../../img/event1.jpg";
 const Timeline = () => (
   <div className="timeline">
     <div className="line" data-aos="zoom-in" data-aos-duration="700"></div>
-    <EventCard photo={EventPhote1} msg="TEST MSG"></EventCard>
+    <EventCard side="left" photo={EventPhote1} title="相遇" date="NOVEMBER, 2018" msg=""></EventCard>
+    <EventCard side="right" photo={EventPhote1} title="第一次約會 - 無敵破壞王2" date="NOVEMBER 29, 2018" msg=""></EventCard>
+    <EventCard side="left" photo={EventPhote1} title="Fish 生日" date="DECEMBER 3, 2018" msg=""></EventCard>
+    <EventCard side="right" photo={EventPhote1} title="正式交往" date="DECEMBER 9, 2018" msg=""></EventCard>
+    <EventCard side="left" photo={EventPhote1} title="提親" date="FEBRUARY 28, 2021" msg=""></EventCard>
+    <EventCard side="right" photo={EventPhote1} title="結婚登記" date="MAY 25, 2021" msg=""></EventCard>
+    <EventCard side="left" photo={EventPhote1} title="喜宴" date="DECEMBER 19, 2021" msg=""></EventCard>
   </div>
 );
 
