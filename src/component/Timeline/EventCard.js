30194b8ae6dd233ba977418b0aa76dd0495f75ca
Sean Tai <ataiishere34@gmail.com>
2021-10-29 01:09:14 +0800

    add flop card css code

--- src/component/Timeline/EventCard.js
+++ src/component/Timeline/EventCard.js
@@ -1,6 +1,9 @@
 const EventCard = (props) => (
   <div className={`event-card ${props.side}`} data-aos="zoom-in" data-aos-duration="700">
-    <div className="event-photo" style={{backgroundImage: `url(${props.photo})`}}></div>
+    <div className="event-photo flop-card">
+      <div className="front" style={{backgroundImage: `url(${props.photo})`}}></div>
+      <div className="back"></div>
+    </div>
     <div className="event-msg">
       <div>{props.title}</div>
       <div>{props.date}</div>
